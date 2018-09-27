b665e17f638b4b0bbee1220bd4f61f4244296254
hyungjunec <sangpon@naver.com>
2018-09-28 00:37:57 +0900

    Problem Solving-1

---
+++ src/Batters.java
@@ -0,0 +1,26 @@
+
+public class Batters {
+	public Vertex[] bats = new Vertex[25];
+	Batters(){
+		
+		
+		
+		bats[1] = new Vertex(1,"Kim Tae Kyun","http://www.statiz.co.kr/player.php?opt=4&sopt=0&name=%EA%B9%80%ED%83%9C%EA%B7%A0&birth=1982-05-29&re=0&da=2&year=2018");
+		bats[2] = new Vertex(2,"Song Kwang Min","http://www.statiz.co.kr/player.php?opt=4&sopt=0&name=%EC%86%A1%EA%B4%91%EB%AF%BC&birth=1983-06-24&re=0&da=2&year=2018");
+		bats[3] = new Vertex(3,"Jared Hoying","http://www.statiz.co.kr/player.php?opt=4&sopt=0&name=%ED%98%B8%EC%9E%89&birth=1989-05-18&re=0&da=2&year=2018");
+		bats[4] = new Vertex(4,"Lee Sung Yeol","http://www.statiz.co.kr/player.php?opt=4&name=%EC%9D%B4%EC%84%B1%EC%97%B4&birth=1984-07-13");
+		bats[5] = new Vertex(5,"Jung Kun Woo", "http://www.statiz.co.kr/player.php?opt=4&sopt=0&name=%EC%A0%95%EA%B7%BC%EC%9A%B0&birth=1982-10-02&re=0&da=2&year=2018");
+		
+		
+	}
+	
+	
+	
+	
+
+
+
+	
+	
+
+}
---
+++ src/Vertex.java
@@ -0,0 +1,26 @@
+
+public class Vertex {
+	int BackNum = -1;
+	
+	int nextNum = -1;
+	
+	int prevNum = -1;
+	
+	String PlayerURl = "";
+	
+	String name = "";
+	
+	
+	
+	
+	
+	Vertex(int Backnum ,String name ,String PlayerURL){
+		this.BackNum= Backnum;
+		
+		this.name = name;
+		
+		this.PlayerURl= PlayerURL;
+		
+	}
+
+}
